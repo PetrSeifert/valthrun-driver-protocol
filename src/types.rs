2b7940df6fa48476f28b2f7df9421f0c8829c7f2
WolverinDEV <git@did.science>
2024-12-09 20:51:02 +0900

    updating commands for handling CR3 shenanigans

--- src/types.rs
+++ src/types.rs
@@ -7,12 +7,16 @@ pub type ProcessId = u32;
 bitflags! {
     #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
     pub struct DriverFeature : u64 {
-        const ProcessModules            = 0x00_00_00_01;
-        const ProcessProtectionKernel   = 0x00_00_00_02;
-        const ProcessProtectionZenith   = 0x00_00_00_04;
+        const ProcessList               = 0x00_00_00_01;
+        const ProcessModules            = 0x00_00_00_02;
+        const ProcessProtectionKernel   = 0x00_00_00_04;
+        const ProcessProtectionZenith   = 0x00_00_00_08;
 
         const MemoryRead                = 0x00_00_01_00;
         const MemoryWrite               = 0x00_00_02_00;
+
+        const DttExplicit               = 0x00_00_10_00;
+        const DttCR3Sshenanigans        = 0x00_00_20_00;
 
         const InputKeyboard             = 0x00_01_00_00;
         const InputMouse                = 0x00_02_00_00;
@@ -31,6 +35,31 @@ pub enum ProcessFilter {
 impl Default for ProcessFilter {
     fn default() -> Self {
         Self::None
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct ProcessInfo {
+    pub process_id: ProcessId,
+    pub image_base_name: [u8; 0x0F],
+}
+
+impl ProcessInfo {
+    pub fn get_image_base_name(&self) -> Option<&str> {
+        utils::fixed_buffer_to_str(&self.image_base_name)
+    }
+
+    pub fn set_image_base_name(&mut self, value: &str) -> bool {
+        utils::str_to_fixed_buffer(&mut self.image_base_name, value)
+    }
+}
+
+impl Default for ProcessInfo {
+    fn default() -> Self {
+        Self {
+            process_id: 0,
+            image_base_name: [0; 0x0F],
+        }
     }
 }
 
@@ -76,4 +105,16 @@ impl Default for MemoryAccessResult {
     fn default() -> Self {
         MemoryAccessResult::ProcessUnknown
     }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum DirectoryTableType {
+    /// Use the process directory table base specified by the system
+    Default,
+
+    /// Manually specify the directory table base for the target process
+    Explicit { directory_table_base: u64 },
+
+    /// Try to mitigate CR3 shenanigans and do not use the directory table base known to the system
+    Cr3Shenanigans,
 }
